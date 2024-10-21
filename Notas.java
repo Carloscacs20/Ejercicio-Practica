b428ada8bf00b1abfe96f0bc8e3e026ecb0baff0
Carloscacs20 <167730021+Carloscacs20@users.noreply.github.com>
2024-10-20 20:46:03 -0500

    Update Notas.java

--- Notas.java
+++ Notas.java
@@ -1,2 +1,36 @@
 public class Notas {
+    private Materia materia;
+    private float calificacion;
+
+    // Constructor por defecto
+    public Nota() {
+    }
+
+    // Constructor con todos los atributos
+    public Nota(Materia materia, float calificacion) {
+        this.materia = materia;
+        this.calificacion = calificacion;
+    }
+
+    // Getters y setters
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    // Método para mostrar la información de la nota
+    public String mostrarNota() {
+        return "Materia: " + this.materia.getNombre() + ", Calificación: " + this.calificacion;
+    }
 }
